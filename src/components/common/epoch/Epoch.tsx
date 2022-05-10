86ffbec9273ec4a695ab172a24a7768c935201be
KangYeJin <kyjin9705@gmail.com>
2022-05-10 11:23:45 +0900

    타임라인 리팩토링

--- src/components/common/epoch/Epoch.tsx
+++ src/components/common/epoch/Epoch.tsx
@@ -1,37 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import ProgressBar from './epoch/ProgressBar';
-import { parseMs } from './epoch/Epoch';
-import { useEpochInfo } from '../../action/queries';
-
-const Timeline = () => {
-  const { data } = useEpochInfo();
-
-  return (
-    <EpochWr>
-      <EpochTit>
-        <span>TimeLine</span>
-      </EpochTit>
-      <EpochBox>
-        <strong>{data?.currentEpoch ?? ''}</strong>
-        <ProgressBar label={`ETA ${parseMs(data?.msUntilNextEpoch ?? 0)}`} value={data?.progress ?? 0} />
-        <strong className="epoch-txt">{data?.currentEpoch ? data?.currentEpoch + 1 : ''}</strong>
-      </EpochBox>
-    </EpochWr>
-  );
-};
-
-export default Timeline;
-
-/* Timeline style */
+import ProgressBar from './ProgressBar';
+import { useEpochInfo } from '../../../action/queries';
 
 const EpochWr = styled.div`
   position: relative;
   width: 100%;
   max-width: 603px;
   min-width: 320px;
+  margin: 0 auto 30px;
   padding: 22px 31px;
+  border: 1px solid #cecece;
+  border-radius: 20px;
   text-align: left;
 
   @media all and (max-width: 979px) {
@@ -102,3 +83,39 @@ const EpochBox = styled.div`
     }
   }
 `;
+
+export const parseMs = (milliseconds: number) => {
+  const days = milliseconds / (24 * 60 * 60 * 1000);
+  const absoluteDays = Math.floor(days);
+
+  const hours = (days - absoluteDays) * 24;
+  const absoluteHours = Math.floor(hours);
+
+  const minutes = (hours - absoluteHours) * 60;
+  const absoluteMinutes = Math.floor(minutes);
+
+  const d = absoluteDays > 0 ? `${absoluteDays}d` : '';
+  const h = absoluteHours > 0 ? `${absoluteHours}h` : '';
+  const m = absoluteMinutes > 0 && absoluteDays < 1 ? `${absoluteMinutes}m` : '';
+
+  return `${d} ${h} ${m} `;
+};
+
+const Epoch = () => {
+  const { data } = useEpochInfo();
+
+  return (
+    <EpochWr>
+      <EpochTit>
+        <span>Epoch</span>
+      </EpochTit>
+      <EpochBox>
+        <strong>{data?.currentEpoch ?? ''}</strong>
+        <ProgressBar label={`ETA ${parseMs(data?.msUntilNextEpoch ?? 0)}`} value={data?.progress ?? 0} />
+        <strong className="epoch-txt">{data?.currentEpoch ? data?.currentEpoch + 1 : ''}</strong>
+      </EpochBox>
+    </EpochWr>
+  );
+};
+
+export default Epoch;
